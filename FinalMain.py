7b0e4e7d122a85535cd0d83b52a43cef08780dc7
Dan Griffin <dcompgriff@gmail.com>
2016-12-04 14:55:25 -0600

    Initial commit

---
+++ FinalMain.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+
+def main():
+    data = np.loadtxt('./fisher.csv', delimiter=',')
+
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
